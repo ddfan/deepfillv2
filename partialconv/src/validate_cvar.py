186e15f0ee6c5800a41b27b50f788adf5dd1d9b3
David Fan <david.fan@gmail.com>
2021-07-13 11:39:28 -0700

    siwtching to batch size 1 fixes toy demo

--- partialconv/src/validate_cvar.py
+++ partialconv/src/validate_cvar.py
@@ -17,16 +17,16 @@ class DistDataset(Dataset):
 
         self.d1 = scipy.stats.norm(-2, 1)
         self.d2 = scipy.stats.norm(1.5, 1)
-        self.x_min = -2
-        self.x_max = 2
-        self.y_min = -2
-        self.y_max = 2
+        self.x_min = -3
+        self.x_max = 5
+        self.y_min = -4
+        self.y_max = 4
         self.n_sample_tries = 100
 
         # generate data
         self.n_samples = n_samples
         # first randomly sample x
-        self.x_samp = np.random.uniform(-1, 1, self.n_samples)
+        self.x_samp = np.random.uniform(self.x_min, self.x_max, self.n_samples)
         self.y_samp = np.zeros(n_samples)
         # next for each x, sample y
         for i, x in enumerate(self.x_samp):
@@ -52,8 +52,14 @@ class DistDataset(Dataset):
         return self.n_samples
 
     def __getitem__(self, index):
+        # x = np.expand_dims(np.random.uniform(self.x_min, self.x_max),axis=0)
+        # y = np.expand_dims(self.sample(x),axis=0)
+        # x = torch.FloatTensor(x)
+        # y = torch.FloatTensor(y)
+
         x = torch.FloatTensor(self.x_samp[index:index + 1])
         y = torch.FloatTensor(self.y_samp[index:index + 1])
+
         alpha = torch.rand(1)
         alpha = alpha * 0.98 + 0.01  # prevent 0 and 1 for numeric stability
 
@@ -84,11 +90,11 @@ class Feedforward(nn.Module):
         hidden3 = self.activation(hidden2)
         output = self.fc3(hidden3)
 
-        # var = output[:,0:1]
-        # cvar_less_var = output[:,1:2]
-        # cvar_less_var = torch.sigmoid(cvar_less_var)
+        var = output[:,0:1]
+        cvar_less_var = output[:,1:2]
+        cvar_less_var = torch.abs(cvar_less_var)
 
-        # output = torch.cat((var, cvar_less_var), dim=1)
+        output = torch.cat((var, cvar_less_var), dim=1)
         # output = self.activation(output)
 
         return output
@@ -107,9 +113,9 @@ class CvarLoss(nn.Module):
 
     def forward(self, y_pred, gt, alpha, output_alpha_plus):
         var = y_pred[:, 0]
-        # cvar_less_var = y_pred[:, 1]
-        # cvar = cvar_less_var + var.detach()
-        cvar = y_pred[:,1]
+        cvar_less_var = y_pred[:, 1]
+        cvar = cvar_less_var + var.detach()
+        # cvar = y_pred[:,1]
 
         var_loss = self.var_huber_loss(gt, var, alpha)
         cvar_calc = self.cvar_calc(gt, var, alpha)
@@ -118,9 +124,9 @@ class CvarLoss(nn.Module):
         monotonic_loss = 0.0
         var_alpha_plus = output_alpha_plus[:,0]
         monotonic_loss += self.monotonic_loss(var_alpha_plus.detach(), var)
-        # cvar_alpha_plus = output_alpha_plus[:,0] + output_alpha_plus[:,1]
-        # cvar_alpha_plus = output_alpha_plus[:,1,:,:]
-        # monotonic_loss += self.monotonic_loss(cvar_alpha_plus.detach(), cvar)
+        cvar_alpha_plus = output_alpha_plus[:,0] + output_alpha_plus[:,1]
+        # cvar_alpha_plus = output_alpha_plus[:,1]
+        monotonic_loss += self.monotonic_loss(cvar_alpha_plus.detach(), cvar)
 
         return self.var_weight * var_loss + self.cvar_weight * cvar_loss + self.mono_weight * monotonic_loss
 
@@ -159,7 +165,7 @@ def main():
     test_dataset = DistDataset(n_samples=100)
 
     dataloader = DataLoader(dataset,
-                           batch_size=2,
+                           batch_size=1,
                            shuffle=True)
 
     test_dataloader = DataLoader(test_dataset,
@@ -168,7 +174,7 @@ def main():
 
     print("building network")
     # create network
-    model = Feedforward(2, 256, 2)
+    model = Feedforward(2, 8, 2)
     criterion = CvarLoss()
     optimizer = torch.optim.Adam(model.parameters())
 
@@ -185,6 +191,7 @@ def main():
             output_alpha_plus = model(x_input, alpha + 0.01)
             loss = criterion(y_pred, y_output, alpha, output_alpha_plus)
             loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), 0.001)
             optimizer.step()
 
             total_loss += loss / float(len(dataloader))
@@ -194,7 +201,7 @@ def main():
         for step, (x_input, y_output, alpha) in enumerate(dataloader):
             y_pred = model(x_input,alpha)
             output_alpha_plus = model(x_input, alpha + 0.01)
-            test_loss += criterion(y_pred.squeeze(), y_output, alpha, output_alpha_plus) / float(len(dataloader))
+            test_loss += criterion(y_pred, y_output, alpha, output_alpha_plus) / float(len(dataloader))
 
         print('Epoch {}: \ttrain: {} \ttest: {}'.format(epoch, total_loss.item(), test_loss.item()))
 
@@ -203,27 +210,28 @@ def main():
     plt.plot(dataset.x_samp, dataset.y_samp, 'k.', markersize=2)
     plt.xlabel('x')
     plt.ylabel('y')
+    plt.xlim(-3,4)
 
     # evaluate network for plotting
     alpha_test = [0.1, 0.5, 0.9]
-    x_input = np.expand_dims(np.linspace(-1, 1, 100), axis=-1)
+    x_input = np.expand_dims(np.linspace(-3, 4, 100), axis=-1)
     x_input = torch.FloatTensor(x_input)
     for alpha_val in alpha_test:
         alpha = alpha_val * torch.ones_like(x_input)
         y_out = model(x_input, alpha)
         var = y_out[:, 0]
-        # cvar = y_out[:,0] + y_out[:,1]
-        cvar = y_out[:, 1]
+        cvar = y_out[:,0] + y_out[:,1]
+        # cvar = y_out[:, 1]
 
         plt.plot(x_input.tolist(), var.tolist(), 'r')
         plt.plot(x_input.tolist(), cvar.tolist(), 'g')
 
     plt.subplot(212)
-    x_lin = [-1, -0.5, 0, 0.5, 1]
+    x_lin = [-2,0,2,4]
     cmap = plt.cm.viridis(np.linspace(0, 1, len(x_lin)))
 
     for i, x in enumerate(x_lin):
-        y_lin = np.linspace(-2, 2, 100)
+        y_lin = np.linspace(-3, 4, 100)
         p_lin = dataset.p(x, y_lin)
         plt.plot(y_lin, p_lin, color=cmap[i])
 
